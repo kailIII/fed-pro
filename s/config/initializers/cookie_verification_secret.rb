5f90141fd0965ee937afb444c96ab1d36ea10831
confiz <confiz@confiz-217.(none)>
2014-03-20 12:53:42 +0500

    Removed s folder

--- s/config/initializers/cookie_verification_secret.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
-# no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = '8e55880ccf01114b23c2349fc0d61012102ebb2b5d0f1630ff5ee28a2a1dfa7fa757a8c5ddaa1a284515e23a50ea4bc6cb5e04e4d94ec4c4a8dd6ca534730007';
